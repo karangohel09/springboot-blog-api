b0340d8aa060866a74aaf4e941b5c408e0709b6e
karan gohel <karangohel2093@gmail.com>
2025-08-29 23:36:35 +0530

    feat: added REST endpoints (GET + POST)

--- src/main/java/com/karan/demo/DemoApplication.java
+++ src/main/java/com/karan/demo/DemoApplication.java
@@ -1,32 +1,12 @@
 package com.karan.demo;
 
-import com.karan.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-	@GetMapping
-    public List<Student> hello(){
-		return List.of(
-				new Student(
-						1L,
-						"karan",
-						"karangohel2093@gmail.com",
-						LocalDate.of(2003, Month.SEPTEMBER,20),
-						21
-				)
-		);
 	}
 }
---
+++ src/main/java/com/karan/demo/student/StudentService.java
@@ -0,0 +1,20 @@
+package com.karan.demo.student;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class StudentService {
+    private final StudentRepository repository;
+
+    public StudentService(StudentRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Student> getStudent(){
+        return repository.findAll();
+    }
+}
